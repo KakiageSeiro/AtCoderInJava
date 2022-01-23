d69505d93a0891a11b2a5cbf32845b32300664b8
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2022-01-24 01:57:13 +0900

    変更が反映されてなかった

--- src/main/java/アルゴリズムと数学_演習問題_018.java
+++ src/main/java/アルゴリズムと数学_演習問題_018.java
@@ -1,45 +1,34 @@
 import java.util.*;
 
-public class ABC235C_ {
+public class アルゴリズムと数学_演習問題_018 {
     public static void main(String[] args) {
         // ■■■■■■■■■■■■■■■■■■■■
 
         Scanner sc = new Scanner(System.in);
 
-        int 数列の長さ = sc.nextInt();
-        int クエリの数 = sc.nextInt();
+        int n品数 = sc.nextInt();
 
-        Map<Integer, List<Integer>> 数と位置リスト = new HashMap<>();
-        for (int i = 0; i < 数列の長さ; i++) {
-            int 数 = sc.nextInt();
+        long 個数_100 = 0;
+        long 個数_200 = 0;
+        long 個数_300 = 0;
+        long 個数_400 = 0;
 
-            if (数と位置リスト.containsKey(数)) {
-                数と位置リスト.get(数).add(i + 1);
-            } else {
-                List<Integer> 位置リスト = new ArrayList<>();
-                位置リスト.add(i + 1);
-                数と位置リスト.put(数, 位置リスト);
+        for (int i = 0; i < n品数; i++) {
+            int k金額 = sc.nextInt();
+            if (k金額 == 100) {
+                個数_100++;
+            } else if (k金額 == 200) {
+                個数_200++;
+            } else if (k金額 == 300) {
+                個数_300++;
+            } else if (k金額 == 400) {
+                個数_400++;
             }
         }
 
-        for (int i = 0; i < クエリの数; i++) {
-            int 目的の数 = sc.nextInt();
-            int 何回目 = sc.nextInt();
+        long pパターン数 = (個数_100 * 個数_400) + (個数_200 * 個数_300);
 
-            if (!数と位置リスト.containsKey(目的の数)) {
-                System.out.println("-1");
-                continue;
-            }
-
-            List<Integer> 位置リスト = 数と位置リスト.get(目的の数);
-            if (位置リスト.size() < 何回目) {
-                System.out.println("-1");
-                continue;
-            }
-
-            System.out.println(位置リスト.get(何回目 - 1));
-        }
-
+        System.out.println(pパターン数);
 
         // ■■■■■■■■■■■■■■■■■■■■
     }
--- src/main/java/アルゴリズムと数学_演習問題_019.java
+++ src/main/java/アルゴリズムと数学_演習問題_019.java
@@ -1,36 +1,63 @@
+import java.math.BigInteger;
 import java.util.*;
 
-public class アルゴリズムと数学_演習問題_018 {
+public class アルゴリズムと数学_演習問題_019 {
+
     public static void main(String[] args) {
         // ■■■■■■■■■■■■■■■■■■■■
 
         Scanner sc = new Scanner(System.in);
 
-        int n品数 = sc.nextInt();
+        int n枚数 = sc.nextInt();
+        long aka赤数 = 0;
+        long ki黄数 = 0;
+        long ao青数 = 0;
 
-        long 個数_100 = 0;
-        long 個数_200 = 0;
-        long 個数_300 = 0;
-        long 個数_400 = 0;
-
-        for (int i = 0; i < n品数; i++) {
-            int k金額 = sc.nextInt();
-            if (k金額 == 100) {
-                個数_100++;
-            } else if (k金額 == 200) {
-                個数_200++;
-            } else if (k金額 == 300) {
-                個数_300++;
-            } else if (k金額 == 400) {
-                個数_400++;
+        for (int i = 0; i < n枚数; i++) {
+            int i色 = sc.nextInt();
+            if (i色 == 1) {
+                aka赤数++;
+            } else if (i色 == 2) {
+                ki黄数++;
+            } else if (i色 == 3) {
+                ao青数++;
             }
         }
 
-        long pパターン数 = (個数_100 * 個数_400) + (個数_200 * 個数_300);
+        long ans = (aka赤数 * (aka赤数 - 1)) / 2 +
+                (ki黄数 * (ki黄数 - 1)) / 2 +
+                (ao青数 * (ao青数 - 1)) / 2;
 
-        System.out.println(pパターン数);
+        System.out.println(ans);
 
-        // ■■■■■■■■■■■■■■■■■■■■
+    }
+
+    private static BigInteger n個のものからr個を選ぶ方法の数(long nLong, long rLong) {
+        if(nLong < rLong){
+            return new BigInteger("0");
+        }
+
+        // nCr = n! / r!(n-r)!
+        // n(n-1)/r
+        BigInteger n = new BigInteger(String.valueOf(nLong));
+        BigInteger r = new BigInteger(String.valueOf(rLong));
+        return factorial階乗(n).divide( // n! / の部分
+                factorial階乗(r).multiply( // r!( の部分
+                        factorial階乗(n.subtract(r)) // n-_r の部分
+                )
+        );
+    }
+
+    // BigIntegerの四則演算
+    // + add
+    // - subtract
+    // * multiply
+    // / divide
+    static BigInteger factorial階乗(BigInteger target) {
+        if (target.longValue() == 0) {
+            return new BigInteger("1");
+        }
+        return target.multiply(factorial階乗(target.subtract(new BigInteger("1"))));
     }
 }
 
